e9d063b11c893303e3e457de203cbb15f138f0c9
yinm <yinm.cminor9@gmail.com>
2018-04-07 18:05:35 +0900

    Check done()

--- 02/spec/appSpec.js
+++ 02/spec/appSpec.js
@@ -1,19 +1,22 @@
 describe('suite', () => {
-  beforeEach(() => {
+  beforeEach((done) => {
     setTimeout(() => {
       console.log('beforeEach')
+      done()
     }, 1000)
   })
 
-  it('spec', () => {
+  it('spec', (done) => {
     setTimeout(() => {
       console.log('spec')
+      done()
     }, 500)
   })
 
-  afterEach(() => {
+  afterEach((done) => {
     setTimeout(() => {
       console.log('afterEach')
+      done()
     }, 200)
   })
 })
