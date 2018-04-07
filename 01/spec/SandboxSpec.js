1ec1e0b4e2b1bfec7ad1d8a57a90fc661628dfd3
yinm <yinm.cminor9@gmail.com>
2018-04-07 10:00:40 +0900

    Check toBe() and toEqual

--- 01/spec/SandboxSpec.js
+++ 01/spec/SandboxSpec.js
@@ -10,4 +10,12 @@ describe('Sandbox', () => {
     expect(sandbox.getBody()).not.toBe(null)
     expect(sandbox.getBody()).toBe('body')
   })
+
+  it("1と'1'", () => {
+    const a = 1
+    expect(a).toEqual(1)
+    expect(a).toBe(1)
+    expect(a).not.toEqual('1')
+    expect(a).not.toBe('1')
+  })
 })
