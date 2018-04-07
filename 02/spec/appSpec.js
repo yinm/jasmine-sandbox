114dceb5d05022e841820be6d0aba36f342718b4
yinm <yinm.cminor9@gmail.com>
2018-04-07 10:39:41 +0900

    Hello world

---
+++ 02/spec/appSpec.js
@@ -0,0 +1,13 @@
+describe('add 関数のテスト', () => {
+  it('1 + 1 は 2', () => {
+    expect(add(1, 1)).toBe(2)
+  })
+
+  it('1 + 4 は 5', () => {
+    expect(add(1, 4)).toBe(5)
+  })
+
+  it('10 + 2 は 12', () => {
+    expect(add(10, 2)).toBe(5)
+  })
+})
