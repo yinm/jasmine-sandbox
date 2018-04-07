685645bcffb25e19642b07d105aedca7a0f4d31f
yinm <yinm.cminor9@gmail.com>
2018-04-07 10:45:55 +0900

    Check this context

--- 02/spec/appSpec.js
+++ 02/spec/appSpec.js
@@ -1,17 +1,25 @@
 describe('test', () => {
   beforeEach(() => {
-    console.log('before >>>')
+    console.log('[beforeEach]')
+    console.log(`this.value = ${this.value}`)
+    this.value = 'before'
   })
 
   afterEach(() => {
-    console.log('<<< after')
+    console.log('[afterEach]')
+    console.log(`this.value = ${this.value}`)
+    this.value = 'after'
   })
 
   it('test1', () => {
-    console.log('test1')
+    console.log('[test1]')
+    console.log(`this.value = ${this.value}`)
+    this.value = 'test1'
   })
 
   it('test2', () => {
-    console.log('test2')
+    console.log('[test2]')
+    console.log(`this.value = ${this.value}`)
+    this.value = 'test2'
   })
 })
