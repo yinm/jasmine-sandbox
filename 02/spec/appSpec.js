450f28b764349db0cd31d0018a6882c8cd11a386
yinm <yinm.cminor9@gmail.com>
2018-04-07 11:35:04 +0900

    Check jasmine.objectContaining()

--- 02/spec/appSpec.js
+++ 02/spec/appSpec.js
@@ -1,13 +1,10 @@
 describe('suite', () => {
   it('spec', () => {
-    function MyClass() {}
-    const myClass = new MyClass()
+    const obj = {
+      hoge: 'HOGE',
+      fuga: 'FUGA',
+    }
 
-    expect(myClass).toEqual(jasmine.any(MyClass))
-    expect({}).toEqual(jasmine.any(Object))
-    expect([]).toEqual(jasmine.any(Array))
-    expect(11).toEqual(jasmine.any(Number))
-    expect('').toEqual(jasmine.any(String))
-    expect(true).toEqual(jasmine.any(Boolean))
+    expect(obj).toEqual(jasmine.objectContaining({fuga: 'FUGA'}))
   })
 })
