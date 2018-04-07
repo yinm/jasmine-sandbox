f47e58738a43c924431cd0023a8e3b069b4a3904
yinm <yinm.cminor9@gmail.com>
2018-04-07 11:15:00 +0900

    Check toBeNull()

--- 02/spec/appSpec.js
+++ 02/spec/appSpec.js
@@ -1,10 +1,5 @@
 describe('suite', () => {
   it('spec', () => {
-    const
-      a = 'aaa',
-      u = undefined
-
-    expect(a).not.toBeUndefined()
-    expect(u).toBeUndefined()
+    expect(null).toBeNull()
   })
 })
