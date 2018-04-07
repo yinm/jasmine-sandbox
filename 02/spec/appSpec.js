e8cf2de7c8a77dbd489da1b051b024ac64c90160
yinm <yinm.cminor9@gmail.com>
2018-04-07 17:55:32 +0900

    Check jasmine.createSpy()

--- 02/spec/appSpec.js
+++ 02/spec/appSpec.js
@@ -1,19 +1,8 @@
 describe('suite', () => {
   it('spec', () => {
-    const obj = {
-      method() {
-        console.log('obj#method()')
-      }
-    }
+    const spyFunction = jasmine.createSpy()
 
-    spyOn(obj, 'method')
-
-    obj.method('first')
-    obj.method(11)
-    obj.method('last')
-
-    // エラーになる。いい感じにmatcherがわからなかった
-    expect(obj.method.calls.first()).toEqual({object: obj, args: ['first']})
-    expect(obj.method.calls.mostRecent()).toEqual({object: obj, args: ['last']})
+    spyFunction()
+    expect(spyFunction).toHaveBeenCalled()
   })
 })
