122a0cae5ae4e87c311c6404eff69c68e69bd2af
yinm <yinm.cminor9@gmail.com>
2018-04-07 10:59:50 +0900

    Check toBe()

--- 02/spec/appSpec.js
+++ 02/spec/appSpec.js
@@ -1,28 +1,5 @@
-describe('suite1', () => {
-  it('spec1', () => {
-    console.log('suite1 spec1')
+describe('suite', () => {
+  it('spec', () => {
+    expect(10).toBe(10)
   })
-
-  xit('spec2', () => {
-    console.log('suite1 spec2')
-  })
-})
-
-xdescribe('suite2', () => {
-  it('spec1', () => {
-    console.log('suite2 spec1')
-  })
-
-  xit('spec2', () => {
-    console.log('suite2 spec2')
-  })
-})
-
-describe('suite3', () => {
-  it('spec1', () => {
-    console.log('suite3 spec1')
-    pending()
-  })
-
-  it('spec2')
 })
