0072e06230585c0ba5d4308a7cb6b2d175a51b4f
yinm <yinm.cminor9@gmail.com>
2018-04-07 11:03:16 +0900

    Check spec of object and array with toEqual()

--- 02/spec/appSpec.js
+++ 02/spec/appSpec.js
@@ -1,5 +1,21 @@
 describe('suite', () => {
   it('spec', () => {
-    expect(10).not.toBe(13)
+    const
+      obj1 = {
+        name: 'Hoge',
+        age: 14,
+      },
+      obj2 = {
+        name: 'Hoge',
+        age: 14,
+      }
+
+    expect(obj1).toEqual(obj2)
+
+    const
+      array1 = [1, 2, 3,],
+      array2 = [1, 2, 3,]
+
+    expect(array1).toEqual(array2)
   })
 })
