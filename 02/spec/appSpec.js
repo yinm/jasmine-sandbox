d8a95280522e1260aa37382741b8e946b38d60ab
yinm <yinm.cminor9@gmail.com>
2018-04-07 11:05:23 +0900

    Check toMatch()

--- 02/spec/appSpec.js
+++ 02/spec/appSpec.js
@@ -1,21 +1,5 @@
 describe('suite', () => {
   it('spec', () => {
-    const
-      obj1 = {
-        name: 'Hoge',
-        age: 14,
-      },
-      obj2 = {
-        name: 'Hoge',
-        age: 14,
-      }
-
-    expect(obj1).toEqual(obj2)
-
-    const
-      array1 = [1, 2, 3,],
-      array2 = [1, 2, 3,]
-
-    expect(array1).toEqual(array2)
+    expect('Hello Jasmine!!').toMatch(/^[a-zA-Z! ]+$/)
   })
 })
