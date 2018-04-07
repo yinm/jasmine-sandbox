5f7578773a69589eea782188d20246ff8e8569df
yinm <yinm.cminor9@gmail.com>
2018-04-07 18:01:12 +0900

    Check jasmine.clock()

--- 02/spec/appSpec.js
+++ 02/spec/appSpec.js
@@ -1,11 +1,17 @@
 describe('suite', () => {
   it('spec', () => {
+    jasmine.clock().install()
+
     const spy = jasmine.createSpy()
 
     setTimeout(() => {
       spy()
-    }, 1000)
+    }, 100)
+
+    jasmine.clock().tick(101)
 
     expect(spy).toHaveBeenCalled()
+
+    jasmine.clock().uninstall()
   })
 })
